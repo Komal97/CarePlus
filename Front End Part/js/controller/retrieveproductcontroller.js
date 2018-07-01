cafcff742ce9699a8904c7a24525b25f8bdb92c6
chhavi97 <chhavi.chhavi97@gmail.com>
2018-07-01 19:55:10 +0530

    data brought dynamically to preview page

--- Front End Part/js/controller/retrieveproductcontroller.js
+++ Front End Part/js/controller/retrieveproductcontroller.js
@@ -1,19 +1,23 @@
 app.controller("retrieveproductcontroller", function ($scope, retrieveproductfactory) {
-  $scope.showdata=function($event,items){
+  $scope.showdata = function ($event, items) {
     console.log("controller");
-   
-     var promise=retrieveproductfactory.showdata($event,items);
-     promise.then(function(data){
-         console.log("data",data.data[0].name);
-       //  $scope.data=data;
-    // $scope.image=data.url;
-    $scope.nameofitem=data.data[0].name;
-   console.log( $scope.nameofitem);
-     },function(err){
-        console.log("error",err);
-       
-     });
- };
+
+    var promise = retrieveproductfactory.showdata($event, items);
+    promise.then(function (data) {
+
+      angular.element(document.querySelector('#previmage')).src=data.data[0].url;
+
+      angular.element(document.querySelector('#prevname')).append('<p>' + data.data[0].name + '</p>');
+
+      angular.element(document.querySelector('#prevprice')).append('<p>' + "Rs. "+ data.data[0].price + '</p>');
+
+      angular.element(document.querySelector('#prevdes')).append('<p>' + data.data[0].description + '</p>');
+
+    }, function (err) {
+      console.log("error", err);
+
+    });
+  };
 
   var promise = retrieveproductfactory.callServer();
   promise.then(function (data) {
--- Front End Part/js/controller/usercontroller.js
+++ Front End Part/js/controller/usercontroller.js
@@ -28,6 +28,5 @@ app.controller("myctrl",function($scope,myfactory,$localStorage){
         });
        
         $('#myModalRegister').modal('hide');
-       
     }
 })
