cc5a38867e66f599501dbc75c89cd37d524a32ee
Komal97 <komalbansal97@gmail.com>
2018-07-06 00:01:06 +0530

    forgot password using nodemailer

---
+++ Back End Part/utils/mail.js
@@ -0,0 +1,30 @@
+const nodemailer=require("nodemailer");
+
+function configmail(userid,password,response){
+    nodemailer.createTestAccount((err,account)=>{
+        let transporter=nodemailer.createTransport({
+            service:'gmail',
+            auth:{
+                user:'komal.bansal.2297@gmail.com',
+                pass:'k1o9m9a7l'
+            }
+        });
+
+        let mailOptions = {
+            from:'komal.bansal.2297@gmail.com',
+            to:userid,
+            subject:"Care+ - Your password details",
+            text:'Your password is '+password+"\n Thank you !!"
+        };
+
+        transporter.sendMail(mailOptions,(error,info)=>{
+            if(error){
+                console.log("Mail not send error",error);
+               // response.json({message:"cant't send mail,some error"});
+            }
+            console.log("mail send successfully");
+           // response.send({message:"mail send successfully"});
+        })
+    })
+}
+module.exports=configmail;
