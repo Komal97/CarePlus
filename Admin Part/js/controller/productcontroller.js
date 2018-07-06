868fbb0b09f80883872cd03b855de4cdfe287545
chhavi97 <chhavi.chhavi97@gmail.com>
2018-07-07 00:22:29 +0530

    master page of admin made and database updated

--- Admin Part/js/controller/productcontroller.js
+++ Admin Part/js/controller/productcontroller.js
@@ -1,10 +1,15 @@
 app.controller("myctrl",function($scope,myfactory){
+
+    $scope.updateprice=function(){
+        $scope.showprice=$scope.price;
+    }
     $scope.addproduct=function(){
         console.log("controller");
         var productobject1=new product($scope.modalno,$scope.name,$scope.price,$scope.url,$scope.description,$scope.quantity,$scope.status,$scope.outofstock,$scope.type);
         var promise=myfactory.addproduct(productobject1);
         promise.then(function(data){
            console.log("Back to promise...",data);
+           alert("product added...");
         },function(err){
            //console.log(err);
         })
